63dcea25e1b07e272bc976d717c7011159e7cd82
YutaManaka <54618797+YutaManaka@users.noreply.github.com>
2023-11-05 21:47:45 +0900

    [fix] base_url修正

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITHUB_PAGES
-    ? '/FragranceFinder/' // レポジトリ名を設定
-    : './'
+  base: 'https://yutamanaka.github.io/FragranceFinder/'
 })
